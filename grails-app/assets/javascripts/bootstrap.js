aa49bd291ebfef345689266b2201773dd9aabd56
Yifat Biezuner <yifat@3baysover.com>
2014-07-16 11:43:46 +0300

    assets

---
+++ grails-app/assets/javascripts/bootstrap.js
@@ -0,0 +1,15 @@
+/**
+ * Created by yifatbiezuner on 10/07/2014.
+ */
+//=require bootstrap-affix
+//=require bootstrap-alert
+//=require bootstrap-button
+//=require bootstrap-carousel
+//=require bootstrap-collapse
+//=require bootstrap-dropdown
+//=require bootstrap-modal
+//=require bootstrap-tooltip
+//=require bootstrap-popover
+//=require bootstrap-scrollspy
+//=require bootstrap-tab
+//=require bootstrap-transition
